81a2cc19a02fd6aba7bde93ba01bb5035448c14e
AlexKat <katsubo79@gmail.com>
2023-12-23 12:42:25 +0200

    2

--- src/components/CountryList/CountryList.jsx
+++ src/components/CountryList/CountryList.jsx
@@ -1,11 +1,16 @@
 import { Grid, GridItem } from 'components';
+import { Link, useLocation } from 'react-router-dom';
+
 
 export const CountryList = ({ countries }) => {
+  const location = useLocation();
   return (
     <Grid>
       {countries.map(({ flag, id }) => (
         <GridItem key={id}>
+          <Link to={`/country/${id}`} state={{from:location}}>
           <img src={flag} alt={id} />
+          </Link>
         </GridItem>
       ))}
     </Grid>
--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,10 +1,37 @@
 import { Section, Container, CountryInfo, Loader } from 'components';
+import { useState, useEffect } from 'react';
+import { Link, useLocation, useParams } from 'react-router-dom';
+import { fetchCountry } from 'service/country-service';
 
 export const Country = () => {
+  
+  const {countryId} = useParams()
+    const [country, setCountry] = useState([]);
+  const [error, setError] = useState(null);
+  const [loader, setLoader] = useState(false);
+  useEffect(() => {
+    const fatchData = async () => {
+      setLoader(true);
+      try {
+        const data = await fetchCountry(countryId);
+        setCountry(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoader(false);
+      }
+    };
+    fatchData();
+  }, [countryId]);
+  console.log(country);
+  const location = useLocation()
+  const goBackLInk = location?.state?.from ?? '/'
+  console.log(location);
   return (
     <Section>
+      <Link to={goBackLInk}>Go Back</Link>
       <Container>
-        <h2>Country</h2>
+        <CountryInfo country={country.countryName } flag={country.flag } capital={country.capital }id={country.id }languages={country.languages }population={country.population }/>
       </Container>
     </Section>
   );
